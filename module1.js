d36c1dd9b06c4a82a2e7b0d03a8160b3b4511e81
Ashley-Pettit <ashley@digital380.com.au>
2016-08-21 15:25:26 +1000

    Automatic commit

--- module1.js
+++ module1.js
@@ -1,3 +1,14 @@
+// var _ = require ('underscore')
+// var m2 = require('./module2');
+// m2();
+// console.log(_)
 
-var m2 = require('./module2')
-console.log('hi')
+var http = require('http')
+
+var server = http.createServer(function(request, response) {
+  console.log('Got a request!')
+  response.write('hi');
+  response.end();
+})
+
+server.listen(3000);
